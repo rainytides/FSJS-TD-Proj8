9b57daa865ff830679c922300e047e5c02f6b2ef
Voke Elstein <141534071+rainytides@users.noreply.github.com>
2023-11-12 02:32:09 -0800

    search and pagination

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require('sequelize');
 
 // Handler function to wrap each route for error handling.
 function asyncHandler(cb) {
@@ -13,16 +14,44 @@ function asyncHandler(cb) {
     };
 }
 
-// GET the full list of books.
+// GET the full list of books with pagination.
 router.get('/', asyncHandler(async (req, res) => {
-    const books = await Book.findAll({ order: [["author", "ASC"]] });
-    res.render('index', { books, title: 'Books' });
+    const page = parseInt(req.query.page) || 1; // Current page
+    const limit = 10; // Books per page
+    const offset = (page - 1) * limit;
+
+    const { count, rows } = await Book.findAndCountAll({
+        limit: limit,
+        offset: offset,
+        order: [["author", "ASC"]]
+    });
+
+    const totalPages = Math.ceil(count / limit);
+
+    res.render('index', { books: rows, title: 'Books', currentPage: page, totalPages: totalPages });
+}));
+
+// Search books
+router.get('/search', asyncHandler(async (req, res) => {
+    const { query } = req.query;
+    const books = await Book.findAll({
+        where: {
+            [Op.or]: [
+                { title: { [Op.like]: `%${query}%` } },
+                { author: { [Op.like]: `%${query}%` } },
+                { genre: { [Op.like]: `%${query}%` } },
+                { year: { [Op.like]: `%${query}%` } }
+            ]
+        },
+        order: [["author", "ASC"]]
+    });
+    res.render('index', { books, title: 'Search Results', query });
 }));
 
 // GET the form for creating a new book.
-router.get('/new', (req, res) => {
+router.get('/new', asyncHandler(async (req, res) => {
     res.render('new-book', { book: {}, title: 'New Book' });
-});
+}));
 
 // POST new book to database.
 router.post('/new', asyncHandler(async (req, res) => {
